bd2ee49db639e27d3ab765471ba2f96955c01e56
Froznar <francisco.marin@ucsp.edu.pe>
2017-04-18 01:30:48 -0500

    first commit

---
+++ Source/Damas/DamasGameModeBase.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Damas.h"
+#include "DamasGameModeBase.h"
+
+
+
+
---
+++ Source/Damas/DamasGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "DamasGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DAMAS_API ADamasGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
